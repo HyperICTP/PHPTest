caf9ae35ac8ab9467505972b6dfae1dd663ce07e
kellkings31 <31912698+kellkings31@users.noreply.github.com>
2017-10-25 11:58:47 +1100

    Presentation Time Test
    Hello Everyone!

--- header.php
+++ header.php
@@ -1,9 +1,9 @@
 <?php
 	echo '
 	<div class="center">
-		<img src="images/hyperscalerssmall.png">
 		<img src="images/redhatsmall.png">
+		<img src="images/hyperscalerssmall.png">
 	</div>
-	Test
+	Hello Panel
 	';
 ?>
